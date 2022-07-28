0c93d6886ad3df77001714de715f8883d9a8bd26
Khwaja Mahad Haq <55731504+KMAHADH@users.noreply.github.com>
2022-07-28 19:19:52 +0500

    Add files via upload

---
+++ MLTraining.java
@@ -0,0 +1,85 @@
+package wineClassification;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.mllib.classification.LogisticRegressionModel;
+import org.apache.spark.mllib.classification.LogisticRegressionWithLBFGS;
+import org.apache.spark.mllib.evaluation.MulticlassMetrics;
+import org.apache.spark.mllib.linalg.Vectors;
+import org.apache.spark.mllib.regression.LabeledPoint;
+
+import scala.Tuple2;
+
+public class MLTraining {
+
+	public static void main(String[] args) {
+	
+		SparkConf conf = new SparkConf().setAppName("WineClassification");
+		
+	    JavaSparkContext jsc = new JavaSparkContext(conf);
+	    
+	    String train_path = "s3n://wine-classification/TrainingDataset.csv";
+		JavaRDD<String> train_data = jsc.textFile(train_path);
+		
+		String first_t = train_data.first();
+	    JavaRDD<String> train_filtered_data = train_data.filter((String s) -> {return !s.contains(first_t);});
+		    
+		JavaRDD<LabeledPoint> train_parsed_data = train_filtered_data.map(line -> {
+		  String[] parts = line.split(";");
+		  double[] points = new double[parts.length - 1];
+	        for (int i = 0; i < (parts.length - 1); i++) {
+	            points[i] = Double.valueOf(parts[i]);
+	        }
+	        return new LabeledPoint(Double.valueOf(parts[parts.length - 1]), Vectors.dense(points));
+	    });
+		
+	
+		train_parsed_data.cache();
+		
+	    String valid_path = "s3n://wine-classification/ValidationDataset.csv";
+		JavaRDD<String> validation_data = jsc.textFile(valid_path);
+		
+		String firstV = validation_data.first();
+	    JavaRDD<String> valid_filtered_data = validation_data.filter((String s) -> {return !s.contains(firstV);});
+	
+		JavaRDD<LabeledPoint> valid_parsed_data = valid_filtered_data.map(line -> {
+		  String[] parts = line.split(";");
+		  double[] points = new double[parts.length - 1];
+	        for (int i = 0; i < (parts.length - 1); i++) {
+	            points[i] = Double.valueOf(parts[i]);
+	        }
+	        return new LabeledPoint(Double.valueOf(parts[parts.length - 1]), Vectors.dense(points));
+	    });
+		
+		valid_parsed_data.cache();
+	
+				LogisticRegressionModel model = new LogisticRegressionWithLBFGS()
+				.setNumClasses(10)
+				.run(train_parsed_data.rdd());
+		
+		JavaPairRDD<Object, Object> predictionAndLabels = valid_parsed_data.mapToPair(p ->
+		  new Tuple2<>(model.predict(p.features()), p.label()));
+
+		MulticlassMetrics metrics = new MulticlassMetrics(predictionAndLabels.rdd());
+		
+		double accuracy = metrics.accuracy();
+		System.out.println();
+		System.out.println("----------------------------------------------------------------------------");
+		System.out.println("Validation Accuracy = " + accuracy);
+		System.out.println("----------------------------------------------------------------------------");
+		System.out.println();
+	
+		double f_score = metrics.weightedFMeasure();
+		System.out.println();
+		System.out.println("----------------------------------------------------------------------------");
+		System.out.println("Validation F Measure = " + f_score);
+		System.out.println("----------------------------------------------------------------------------");
+		System.out.println();
+		model.save(jsc.sc(), "s3n://wine-classification/LogisticRegressionModel");
+		jsc.stop();
+
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+## Overview
+ To build a  "Wine Quality prediction Machine Learning model in Spark over AWS which must be trained in parallel using 4 EC2 instances.
+
+## Project end results:
+This project focuses on developing parallel machine learning (ML) applications in Amazon
+AWS cloud platform.
+The requirements are: 
+
+(a) Use of Apache Spark to train an ML model in parallel on multiple EC2 instances; 
+(b) Using Spark’s MLlib to develop and use an ML model in the cloud; 
+(c) Using Docker to create a container.
+
+## Project specifics:
+
+(1) Input for model training: 
+    (a) TrainingDataset.csv (for model training.)  
+    (b) ValidationDataset.csv (for model validation.)
+
+(2) Input for prediction testing: 
+    (a) TestDataset.csv (the actual dataset)
+
+(3) Output: 
+    (a) The measure of the prediction performance is the F1 score that's generated. 
+
+(4) Model Implementation: 
+To develop a Spark application that uses MLlib to train for wine quality prediction using the Sampldatasets for training.  
+
